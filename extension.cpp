3392614904f10a227d48c261dbc15580516799d2
Mikusch <mikuschger@gmail.com>
2022-10-27 11:13:24 +0200

    Initial commit

---
+++ extension.cpp
@@ -0,0 +1,281 @@
+#include "extension.h"
+#include "ISDKTools.h"
+
+CGlobalVars* gpGlobals = nullptr;
+
+HandleType_t g_IntervalTimerHandleType;
+HandleType_t g_CountdownTimerHandleType;
+CIntervalTimerHandler g_IntervalTimerHandler;
+CCountdownTimerHandler g_CountdownTimerHandler;
+
+MoreTimers g_MoreTimers;
+SMEXT_LINK(&g_MoreTimers);
+
+IntervalTimer* GetIntervalTimerFromHandle(cell_t cellhandle, IPluginContext* pContext)
+{
+	Handle_t handle = (Handle_t)cellhandle;
+	HandleError hndlError;
+	HandleSecurity hndlSecurity;
+
+	hndlSecurity.pOwner = nullptr;
+	hndlSecurity.pIdentity = myself->GetIdentity();
+
+	IntervalTimer* pIntervalTimer;
+	if ((hndlError = g_pHandleSys->ReadHandle(handle, g_IntervalTimerHandleType, &hndlSecurity, (void**)&pIntervalTimer)) != HandleError_None)
+	{
+		if (!pContext)
+		{
+			smutils->LogError(myself, "Invalid IntervalTimer handle %x (error %d)", handle, hndlError);
+		}
+		else
+		{
+			pContext->ThrowNativeError("Invalid IntervalTimer handle %x (error %d)", handle, hndlError);
+		}
+
+		return nullptr;
+	}
+
+	return pIntervalTimer;
+}
+
+CountdownTimer* GetCountdownTimerFromHandle(cell_t cellhandle, IPluginContext* pContext)
+{
+	Handle_t handle = (Handle_t)cellhandle;
+	HandleError hndlError;
+	HandleSecurity hndlSecurity;
+
+	hndlSecurity.pOwner = nullptr;
+	hndlSecurity.pIdentity = myself->GetIdentity();
+
+	CountdownTimer* pCountdownTimer;
+	if ((hndlError = g_pHandleSys->ReadHandle(handle, g_CountdownTimerHandleType, &hndlSecurity, (void**)&pCountdownTimer)) != HandleError_None)
+	{
+		if (!pContext)
+		{
+			smutils->LogError(myself, "Invalid CountdownTimer handle %x (error %d)", handle, hndlError);
+		}
+		else
+		{
+			pContext->ThrowNativeError("Invalid CountdownTimer handle %x (error %d)", handle, hndlError);
+		}
+
+		return nullptr;
+	}
+
+	return pCountdownTimer;
+}
+
+void MoreTimers::SDK_OnAllLoaded()
+{
+	if (!handlesys->FindHandleType("IntervalTimer", &g_IntervalTimerHandleType))
+	{
+		g_IntervalTimerHandleType = handlesys->CreateType("IntervalTimer", &g_IntervalTimerHandler, 0, nullptr, nullptr, myself->GetIdentity(), nullptr);
+	}
+
+	if (!handlesys->FindHandleType("CountdownTimer", &g_CountdownTimerHandleType))
+	{
+		g_CountdownTimerHandleType = handlesys->CreateType("CountdownTimer", &g_CountdownTimerHandler, 0, nullptr, nullptr, myself->GetIdentity(), nullptr);
+	}
+
+	sharesys->AddNatives(myself, MoreTimersNative::g_ExtensionNatives);
+	sharesys->RegisterLibrary(myself, "moretimers");
+}
+
+bool MoreTimers::SDK_OnMetamodLoad(ISmmAPI* ismm, char* error, size_t maxlen, bool late)
+{
+	gpGlobals = ismm->GetCGlobals();
+
+	return true;
+}
+
+namespace MoreTimersNative
+{
+	static cell_t IntervalTimer_Create(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = new IntervalTimer();
+
+		if (!pIntervalTimer)
+		{
+			return 0;
+		}
+
+		return handlesys->CreateHandle(g_IntervalTimerHandleType, pIntervalTimer, pContext->GetIdentity(), myself->GetIdentity(), NULL);
+	}
+
+	static cell_t IntervalTimer_Reset(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			pIntervalTimer->Reset();
+		}
+
+		return 0;
+	}
+
+	static cell_t IntervalTimer_Start(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			pIntervalTimer->Start();
+		}
+
+		return 0;
+	}
+
+	static cell_t IntervalTimer_Invalidate(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			pIntervalTimer->Invalidate();
+		}
+
+		return 0;
+	}
+
+	static cell_t IntervalTimer_HasStarted(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			return pIntervalTimer->HasStarted();
+		}
+
+		return 0;
+	}
+
+	static cell_t IntervalTimer_GetElapsedTimed(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			return sp_ftoc(pIntervalTimer->GetElapsedTime());
+		}
+
+		return 0;
+	}
+
+	static cell_t IntervalTimer_IsLessThan(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			return pIntervalTimer->IsLessThan(sp_ctof(params[2]));
+		}
+
+		return 0;
+	}
+
+	static cell_t IntervalTimer_IsGreaterThan(IPluginContext* pContext, const cell_t* params)
+	{
+		IntervalTimer* pIntervalTimer = GetIntervalTimerFromHandle(params[1], pContext);
+		if (pIntervalTimer)
+		{
+			return pIntervalTimer->IsGreaterThan(sp_ctof(params[2]));
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_Create(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = new CountdownTimer();
+
+		if (!pCountdownTimer)
+		{
+			return 0;
+		}
+
+		return handlesys->CreateHandle(g_CountdownTimerHandleType, pCountdownTimer, pContext->GetIdentity(), myself->GetIdentity(), NULL);
+	}
+
+	static cell_t CountdownTimer_Reset(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			pCountdownTimer->Reset();
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_Start(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			pCountdownTimer->Start(sp_ctof(params[2]));
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_Invalidate(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			pCountdownTimer->Invalidate();
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_HasStarted(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			return pCountdownTimer->HasStarted();
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_IsElapsed(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			return pCountdownTimer->IsElapsed();
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_GetElapsedTime(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			return sp_ftoc(pCountdownTimer->GetElapsedTime());
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_GetRemainingTime(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			return sp_ftoc(pCountdownTimer->GetRemainingTime());
+		}
+
+		return 0;
+	}
+
+	static cell_t CountdownTimer_GetCountdownDuration(IPluginContext* pContext, const cell_t* params)
+	{
+		CountdownTimer* pCountdownTimer = GetCountdownTimerFromHandle(params[1], pContext);
+		if (pCountdownTimer)
+		{
+			return sp_ftoc(pCountdownTimer->GetCountdownDuration());
+		}
+
+		return 0;
+	}
+}
---
+++ extension.h
@@ -0,0 +1,264 @@
+#ifndef _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
+#define _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
+
+#include "smsdk_ext.h"
+
+extern CGlobalVars* gpGlobals;
+
+//--------------------------------------------------------------------------------------------------------------
+class CIntervalTimerHandler : public IHandleTypeDispatch
+{
+public:
+	void OnHandleDestroy(HandleType_t type, void* object)
+	{
+		CIntervalTimerHandler* pIntervalTimerHandler = (CIntervalTimerHandler*)object;
+		delete pIntervalTimerHandler;
+	}
+};
+
+//--------------------------------------------------------------------------------------------------------------
+class CCountdownTimerHandler : public IHandleTypeDispatch
+{
+public:
+	void OnHandleDestroy(HandleType_t type, void* object)
+	{
+		CCountdownTimerHandler* pCountdownTimerHandler = (CCountdownTimerHandler*)object;
+		delete pCountdownTimerHandler;
+	}
+};
+
+//--------------------------------------------------------------------------------------------------------------
+/**
+ * Simple class for tracking intervals of game time.
+ * Upon creation, the timer is invalidated.  To measure time intervals, start the timer via Start().
+ */
+class IntervalTimer
+{
+public:
+	IntervalTimer(void)
+	{
+		m_timestamp = -1.0f;
+	}
+
+	void Reset(void)
+	{
+		m_timestamp = gpGlobals->curtime;
+	}
+
+	void Start(void)
+	{
+		m_timestamp = gpGlobals->curtime;
+	}
+
+	void Invalidate(void)
+	{
+		m_timestamp = -1.0f;
+	}
+
+	bool HasStarted(void) const
+	{
+		return (m_timestamp > 0.0f);
+	}
+
+	/// if not started, elapsed time is very large
+	float GetElapsedTime(void) const
+	{
+		return (HasStarted()) ? (gpGlobals->curtime - m_timestamp) : 99999.9f;
+	}
+
+	bool IsLessThan(float duration) const
+	{
+		return (gpGlobals->curtime - m_timestamp < duration) ? true : false;
+	}
+
+	bool IsGreaterThan(float duration) const
+	{
+		return (gpGlobals->curtime - m_timestamp > duration) ? true : false;
+	}
+
+private:
+	float m_timestamp;
+};
+
+//--------------------------------------------------------------------------------------------------------------
+/**
+ * Simple class for counting down a short interval of time.
+ * Upon creation, the timer is invalidated.  Invalidated countdown timers are considered to have elapsed.
+ */
+class CountdownTimer
+{
+public:
+	CountdownTimer(void)
+	{
+		m_timestamp = -1.0f;
+		m_duration = 0.0f;
+	}
+
+	void Reset(void)
+	{
+		m_timestamp = gpGlobals->curtime + m_duration;
+	}
+
+	void Start(float duration)
+	{
+		m_timestamp = gpGlobals->curtime + duration;
+		m_duration = duration;
+	}
+
+	void Invalidate(void)
+	{
+		m_timestamp = -1.0f;
+	}
+
+	bool HasStarted(void) const
+	{
+		return (m_timestamp > 0.0f);
+	}
+
+	bool IsElapsed(void) const
+	{
+		return (gpGlobals->curtime > m_timestamp);
+	}
+
+	float GetElapsedTime(void) const
+	{
+		return gpGlobals->curtime - m_timestamp + m_duration;
+	}
+
+	float GetRemainingTime(void) const
+	{
+		return (m_timestamp - gpGlobals->curtime);
+	}
+
+	/// return original countdown time
+	float GetCountdownDuration(void) const
+	{
+		return (m_timestamp > 0.0f) ? m_duration : 0.0f;
+	}
+
+private:
+	float m_duration;
+	float m_timestamp;
+};
+
+//--------------------------------------------------------------------------------------------------------------
+class MoreTimers : public SDKExtension
+{
+public:
+	/**
+	 * @brief This is called after the initial loading sequence has been processed.
+	 *
+	 * @param error		Error message buffer.
+	 * @param maxlen	Size of error message buffer.
+	 * @param late		Whether or not the module was loaded after map load.
+	 * @return			True to succeed loading, false to fail.
+	 */
+	 //virtual bool SDK_OnLoad(char* error, size_t maxlen, bool late);
+
+	 /**
+	  * @brief This is called right before the extension is unloaded.
+	  */
+	  //virtual void SDK_OnUnload();
+
+	  /**
+	   * @brief This is called once all known extensions have been loaded.
+	   * Note: It is is a good idea to add natives here, if any are provided.
+	   */
+	virtual void SDK_OnAllLoaded();
+
+	/**
+	 * @brief Called when the pause state is changed.
+	 */
+	 //virtual void SDK_OnPauseChange(bool paused);
+
+	 /**
+	  * @brief this is called when Core wants to know if your extension is working.
+	  *
+	  * @param error		Error message buffer.
+	  * @param maxlen	Size of error message buffer.
+	  * @return			True if working, false otherwise.
+	  */
+	  //virtual bool QueryRunning(char *error, size_t maxlen);
+public:
+#if defined SMEXT_CONF_METAMOD
+	/**
+	 * @brief Called when Metamod is attached, before the extension version is called.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @param late			Whether or not Metamod considers this a late load.
+	 * @return				True to succeed, false to fail.
+	 */
+	virtual bool SDK_OnMetamodLoad(ISmmAPI* ismm, char* error, size_t maxlen, bool late);
+
+	/**
+	 * @brief Called when Metamod is detaching, after the extension version is called.
+	 * NOTE: By default this is blocked unless sent from SourceMod.
+	 *
+	 * @param error			Error buffer.
+	 * @param maxlen		Maximum size of error buffer.
+	 * @return				True to succeed, false to fail.
+	 */
+	 //virtual bool SDK_OnMetamodUnload(char *error, size_t maxlen);
+
+	 /**
+	  * @brief Called when Metamod's pause state is changing.
+	  * NOTE: By default this is blocked unless sent from SourceMod.
+	  *
+	  * @param paused		Pause state being set.
+	  * @param error			Error buffer.
+	  * @param maxlen		Maximum size of error buffer.
+	  * @return				True to succeed, false to fail.
+	  */
+	  //virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlen);
+#endif
+};
+
+//--------------------------------------------------------------------------------------------------------------
+namespace MoreTimersNative
+{
+	static cell_t IntervalTimer_Create(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_Reset(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_Start(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_Invalidate(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_HasStarted(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_GetElapsedTimed(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_IsLessThan(IPluginContext* pContext, const cell_t* params);
+	static cell_t IntervalTimer_IsGreaterThan(IPluginContext* pContext, const cell_t* params);
+
+	static cell_t CountdownTimer_Create(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_Reset(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_Start(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_Invalidate(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_HasStarted(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_IsElapsed(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_GetElapsedTime(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_GetRemainingTime(IPluginContext* pContext, const cell_t* params);
+	static cell_t CountdownTimer_GetCountdownDuration(IPluginContext* pContext, const cell_t* params);
+
+	const sp_nativeinfo_t g_ExtensionNatives[] =
+	{
+			{"IntervalTimer.IntervalTimer", IntervalTimer_Create},
+			{"IntervalTimer.Reset", IntervalTimer_Reset},
+			{"IntervalTimer.Start", IntervalTimer_Start},
+			{"IntervalTimer.Invalidate", IntervalTimer_Invalidate},
+			{"IntervalTimer.HasStarted", IntervalTimer_HasStarted},
+			{"IntervalTimer.GetElapsedTime", IntervalTimer_GetElapsedTimed},
+			{"IntervalTimer.IsLessThan", IntervalTimer_IsLessThan},
+			{"IntervalTimer.IsGreaterThan", IntervalTimer_IsGreaterThan},
+
+			{"CountdownTimer.CountdownTimer", CountdownTimer_Create},
+			{"CountdownTimer.Reset", CountdownTimer_Reset},
+			{"CountdownTimer.Start", CountdownTimer_Start},
+			{"CountdownTimer.Invalidate", CountdownTimer_Invalidate},
+			{"CountdownTimer.HasStarted", CountdownTimer_HasStarted},
+			{"CountdownTimer.IsElapsed", CountdownTimer_IsElapsed},
+			{"CountdownTimer.GetElapsedTime", CountdownTimer_GetElapsedTime},
+			{"CountdownTimer.GetRemainingTime", CountdownTimer_GetRemainingTime},
+			{"CountdownTimer.GetCountdownDuration", CountdownTimer_GetCountdownDuration},
+
+			{NULL, NULL},
+	};
+}
+
+#endif // _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
